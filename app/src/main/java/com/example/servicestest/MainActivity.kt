a09d1a6a4f72cb96e5af8887592a403afb4ad07b
RedMuri <kartoev.04@bk.ru>
2022-10-13 13:05:12 +0300

    work manager

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -13,6 +13,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import com.example.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -27,26 +30,34 @@ class MainActivity : AppCompatActivity() {
             startService(MyService.newIntent(this))
         }
         binding.foregroundService.setOnClickListener {
-            ContextCompat.startForegroundService(this,MyForegroundService.newIntent(this))
+            ContextCompat.startForegroundService(this, MyForegroundService.newIntent(this))
         }
         binding.intentService.setOnClickListener {
-            ContextCompat.startForegroundService(this,MyIntentService.newIntent(this))
+            ContextCompat.startForegroundService(this, MyIntentService.newIntent(this))
         }
         binding.jobScheduler.setOnClickListener {
-            val componentName = ComponentName(this,MyJobService::class.java)
-            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID,componentName)
+            val componentName = ComponentName(this, MyJobService::class.java)
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
                 .setRequiresCharging(true)
                 .build()
             val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
             val intent = MyJobService.newIntent(page++)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
-            } else{
-                startService(MyIntentService2.newIntent(this,page++))
+            } else {
+                startService(MyIntentService2.newIntent(this, page++))
             }
         }
         binding.jobIntentService.setOnClickListener {
-            MyJobIntentService.enqueue(this,page++)
+            MyJobIntentService.enqueue(this, page++)
+        }
+        binding.workManager.setOnClickListener {
+            val workManager = WorkManager.getInstance(applicationContext)
+            workManager.enqueueUniqueWork(
+                MyWorker.WORK_NAME,
+                ExistingWorkPolicy.APPEND,
+                MyWorker.makeRequest(page++)
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/servicestest/MyWorker.kt
@@ -0,0 +1,42 @@
+package com.example.servicestest
+
+import android.content.Context
+import android.util.Log
+import androidx.work.*
+
+class MyWorker(
+    context: Context,
+    private val workerParams: WorkerParameters,
+) : androidx.work.Worker(context, workerParams) {
+
+    override fun doWork(): Result {
+        log("doWork")
+        val page = workerParams.inputData.getInt(PAGE,0)
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i $page")
+        }
+        return Result.success()
+    }
+
+    private fun log(message: String) {
+        Log.d("muri", "MyWorker: $message")
+    }
+
+    companion object{
+
+        private const val PAGE = "page"
+        const val WORK_NAME = "work name"
+
+        fun makeRequest(page: Int): OneTimeWorkRequest {
+            return OneTimeWorkRequestBuilder<MyWorker>()
+                .setInputData(workDataOf(PAGE to page))
+                .setConstraints(makeConstraints())
+                .build()
+        }
+
+        private fun makeConstraints() = Constraints.Builder()
+            .setRequiresCharging(true)
+            .build()
+    }
+}
